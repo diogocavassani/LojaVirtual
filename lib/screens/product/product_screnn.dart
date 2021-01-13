b3f231c8c397158f6d165a80c96eddb4d3fc8e13
Diogo <diogocavassani@gmail.com>
2021-01-12 21:18:50 -0300

    Selecao do tamanho

--- lib/screens/product/product_screnn.dart
+++ lib/screens/product/product_screnn.dart
@@ -1,6 +1,9 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/product.dart';
+import 'package:loja_virtual/models/user_manager.dart';
+import 'package:loja_virtual/screens/product/components/size_widget.dart';
+import 'package:provider/provider.dart';
 
 class ProdutcScreen extends StatelessWidget {
   const ProdutcScreen(this.product);
@@ -8,70 +11,117 @@ class ProdutcScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1,
-            child: Carousel(
-              images: product.images.map((url) {
-                return NetworkImage(url);
-              }).toList(),
-              dotSize: 4,
-              dotBgColor: Colors.transparent,
-              dotColor: primaryColor,
-              autoplay: false,
+    return ChangeNotifierProvider.value(
+      value: product,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(product.name),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: ListView(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1,
+              child: Carousel(
+                images: product.images.map((url) {
+                  return NetworkImage(url);
+                }).toList(),
+                dotSize: 4,
+                dotBgColor: Colors.transparent,
+                dotColor: primaryColor,
+                autoplay: false,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  product.name,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Text(
-                    'A partir de',
-                    style: TextStyle(color: Colors.grey[600], fontSize: 13),
-                  ),
-                ),
-                Text(
-                  'R\$ 19,99',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: primaryColor),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16, bottom: 8),
-                  child: Text(
-                    'Descrição',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
                     ),
                   ),
-                ),
-                Text(
-                  product.description,
-                  style: const TextStyle(fontSize: 16),
-                )
-              ],
-            ),
-          )
-        ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      'A partir de',
+                      style: TextStyle(color: Colors.grey[600], fontSize: 13),
+                    ),
+                  ),
+                  Text(
+                    'R\$ 19,99',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: primaryColor),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 8),
+                    child: Text(
+                      'Descrição',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    product.description,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 8),
+                    child: Text(
+                      'Tamanhos',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: product.sizes.map((s) {
+                      return SizeWidget(size: s);
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 20),
+                  if (product.hasStock)
+                    Consumer2<UserManager, Product>(
+                      builder: (_, userManager, product, __) {
+                        return SizedBox(
+                          height: 44,
+                          child: RaisedButton(
+                            onPressed: product.selectedSize != null
+                                ? () {
+                                    if (userManager.isLoggedIn) {
+                                      //TODO: Adicionar ao Carrinho
+                                    } else {
+                                      Navigator.of(context).pushNamed('/login');
+                                    }
+                                  }
+                                : null,
+                            color: primaryColor,
+                            child: Text(
+                              userManager.isLoggedIn
+                                  ? 'Adicionar ao Carrinho'
+                                  : 'Entre para Comprar',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
